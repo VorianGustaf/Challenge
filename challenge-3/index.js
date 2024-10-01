4354f7f814c17db96d7dbdfcba14c606c9c51cc9
VorianGustaf <voriangustafs@gmail.com>
2024-10-01 13:21:58 +0700

    fungsi dimasukan filter

---
+++ challenge-3/index.js
@@ -0,0 +1,13 @@
+// fungsi find
+const numbers = [4, 9, 16, 25];
+const result = numbers.find(function(number) {
+    return number > 10;
+  });
+  console.log(result);
+
+// fungsi filter
+const angka = [1, 3, 6, 8, 11];
+const hasil =angka.filter(function(angka) {
+  return angka > 5;
+});
+console.log(hasil); // 
