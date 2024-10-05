c66206bfb5fc4bc64974f0ced691ad0c96246234
VorianGustaf <voriangustafs@gmail.com>
2024-10-05 18:49:09 +0700

    bahasa

--- challenge-3/index.js
+++ challenge-3/index.js
@@ -1,72 +1,86 @@
-// fungsi find
-const numbers = [4, 9, 16, 25];
-const result = numbers.find(function(number) {
-    return number > 10;
-  });
-  console.log(result);
+// // fungsi find
+// const numbers = [4, 9 ,16, 25];
+// const result = numbers.find(function(number) {
+//     return number > 100;
+//   });
+//   console.log(result);
 
 // fungsi filter
-const angka = [1, 3, 6, 8, 11];
-const hasil =angka.filter(function(angka) {
-  return angka > 5;
-});
-console.log(hasil);
+// const angka = [1, 3, "6", true, 11];
+// const hasil =angka.filter(function(angka) {
+//   console.log(this);
+//   return angka > 50; 
 
-//  fungsi findInddex
-const nomor =[2,5,6,9,12];
-const isLargeNumber = (element) => element > 6;
+// });
+// console.log(hasil);
 
-console.log(nomor.findIndex(isLargeNumber));
+// //  fungsi findInddex
+// const nomor =[2,5,6,9,12];
+// const isLargeNumber = (element) => {
+//   console.log(this);
+//   return element > 6
+// };
 
-// unshif
-const array1 = [1, 2, 3];
+// console.log(nomor.findIndex(isLargeNumber));
 
-console.log(array1.unshift( 7,8,9));
+// // unshif
+// const array1 = [1, 2, 3];
 
-console.log(array1);
+// console.log(array1.unshift( 7,8,9));
 
-//
-const menuItems = ["Apple Juice", "White Coffee", "Fried Rice", "Croissant"];
-const newMenuItem = "Waffle";
-const penambahan =Math.ceil(menuItems.length /1);
-menuItems.splice(penambahan,0,newMenuItem);
-console.log(menuItems);
+// console.log(array1);
+
+// //
+// const menuItems = ["Apple Juice", "White Coffee", "Fried Rice", "Croissant"];
+// const newMenuItem = "Waffle";
+// const penambahan =Math.floor(menuItems.length /1);
+// menuItems.splice(penambahan,0,newMenuItem);
+// console.log(menuItems);
 
 
-//pop
-const items = ["Apple Juice", "White Coffee", "Fried Rice", "Croissant"];
-const lastItem =items.pop();
-console.log(items);
-// console.log(lastItem);
+// //pop
+// const items = ["Apple Juice", "White Coffee", "Fried Rice", "Croissant"];
+// // const lastItem =items.pop();
+// // console.log(items);
+// // console.log(lastItem);
+// console.log(items.at(-1));
 
 //menghapus elemen-elemen di tengah
-const fruits = [
-  "Apple",
-  "Grape",
-  "Guava",
-  "Raspberry",
-  "Almond",
-  "Vine",
-  "Apricot",
-  "Tangerine",
-  "Elderberry"
-];
-// cara 1 mengunakan splice
+// const fruits = [
+//   "Apple",
+//   "Grape",
+//   "Guava",
+//   "Raspberry",
+//   "Almond",
+//   "Vine",
+//   "Apricot",
+//   "Tangerine",
+//   "Elderberry"
+// ];
+// // cara 1 mengunakan splice
 
-fruits.splice (4,1);
-fruits.splice (4,1);
-console.log(fruits);
+// // fruits.splice 4,1);
+// // fruits.splice (4,1);
+// // console.log(fruits);
 
-//cara 2 mengunakan splice dan indexof
+// //cara 2 mengunakan splice dan indexof
 
-const indexToRemove1 = fruits.indexOf("Almond");
-if (indexToRemove1 !== -1) {
-    fruits.splice(indexToRemove1, 1);
-}
+// const indexToRemove1 = fruits.indexOf("Almond");
+// if (indexToRemove1 !== "-1") {
+//     fruits.splice(indexToRemove1, 1);
+//     console.log("indexToRemove1");
+// }
 
-const indexToRemove2 = fruits.indexOf("Vine");
-if (indexToRemove2 !== -1) {
-    fruits.splice(indexToRemove2, 1);
-}
+// const indexToRemove2 = fruits.indexOf("Almond");
+// if (indexToRemove2 != "-1") {
+//     fruits.splice(indexToRemove2, 1);
+//     console.log("indexToRemove2");
+// }
 
-console.log(fruits);
+// console.log(fruits);
+
+let increment = 1;
+
+console.log(++increment);
+console.log(increment++);
+console.log(increment);
